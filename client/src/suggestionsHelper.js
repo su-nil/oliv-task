ee937d5614037acac32d447034ee9bb6747e3b75
sudorock <kslvsunil@gmail.com>
2019-08-15 00:18:32 +0530

    removed env, going to branch out to try env

--- client/src/suggestionsHelper.js
+++ client/src/suggestionsHelper.js
@@ -1,10 +1,9 @@
 import axios from 'axios';
-const PATH = process.env.SERVER_URL || 'http://localhost:8000';
 
 export default function getSuggestions(query) {
 	return new Promise((resolve, reject) => {
 		axios
-			.get(`${PATH}/autocomplete/${query}`)
+			.get(`http://localhost:8000/autocomplete/${query}`)
 			.then((response) => {
 				const places = response.data.map((place) => {
 					return {
