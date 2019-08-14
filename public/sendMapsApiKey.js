52f8ed6d07e322d54566318a02db4271e7e36088
sudorock <kslvsunil@gmail.com>
2019-08-15 03:13:33 +0530

    removed unused files

--- public/sendMapsApiKey.js
+++
@@ -1,9 +0,0 @@
-// TODO Try catch
-
-const config = require('./config');
-
-module.exports = function(app, db) {
-	app.get('/getapikey', (req, res) => {
-		res.json({ mapsApiKey: config.mapsApiKey });
-	});
-};
